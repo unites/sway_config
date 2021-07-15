9867d712efe6e04729cbd6270e481ab6983b9ede
unites <jaredbryngelson@gmail.com>
2021-07-15 09:43:00 -0500

    up

--- arch_install.sh
+++ arch_install.sh
@@ -1,3 +1,80 @@
+pacman -Sy sway \
+powerline-fonts \
+feh \
+wofi \
+fzf \
+zsh \
+git \
+curl \
+wget \
+tar \
+rsync \
+cifs-utils \
+light \
+ranger \
+vim \
+nautilus \
+seahorse \
+libsecret \
+python3 \
+ruby \
+nodejs \
+pulseaudio \
+pavucontrol \
+mpc \
+mpd \
+ncmpcpp \
+mpv \
+ffmpeg \
+freerdp \
+syncthing \
+gnome-bluetooth \
+gnome-logs \
+blueman \
+ksystemlog \
+gimp \
+id3v2 \
+pciutils \
+gnome-system-monitor \
+ksystemlog \
+vinagre \
+i3 \
+i3status \
+dmenu \
+i3lock \
+conky \
+gnome-connections \
+kbd \
+rofi \
+xss-lock \
+xautolock \
+picom \
+nitrogen \
+maim \
+xclip \
+dunst \
+gnome-icon-theme \
+neofetch \
+imagemagick \
+bind-tools \
+lchsh \
+perl-anyevent-i3 \
+xorg \
+xorg-xinit \
+alacritty
 
+# Nvidia Drivers
+pacman -Sy nvidia \
+nvidia-settings \
 
-perl-anyevent-i3 
+yay -S microsoft-edge-dev \
+brave-bin \
+brave-beta-bin \
+brave-nightly-bin \
+google-chrome
+
+# Flatpak
+# Steam, Discord, Firefox, Obsidian, Teams, visual studio code, obs, vlc, chromium, inkscape, Wike, GNOME Chess
+
+# pacman -Sy vlc 
+
